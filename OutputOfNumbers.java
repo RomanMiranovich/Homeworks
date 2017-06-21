df58e8bde8cb1d804f3cdc5751bdd1aa78220eff
RomanMiranovich <roman.miranovich@gmail.com>
2017-06-21 20:03:16 +0200

    Update OutputOfNumbers.java

--- OutputOfNumbers.java
+++ OutputOfNumbers.java
@@ -1,18 +1,18 @@
 public class OutputOfNumbers {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
-            if ((i % 3 == 0) && (i % 5 != 0)) {
-                System.out.println("Tutti");
-            }  
-            if ((i % 5 == 0) && (i % 3 != 0)) {
-                System.out.println("Frutti");
-            } 
-            if ((i % 3 == 0) && (i % 5 == 0)) {
-                System.out.println("Tutti-Frutti");
-            } 
-            if ((i % 3 != 0) && (i % 5 != 0)) {
-                System.out.println(i);
-            }		
-        }		
-    }
+  public static void main(String[] args) {
+    for (int i = 0; i <= 100; i++) {
+      if ((i % 3 == 0) && (i % 5 != 0)) {
+        System.out.println("Tutti");
+      }  
+      if ((i % 5 == 0) && (i % 3 != 0)) {
+        System.out.println("Frutti");
+      } 
+      if ((i % 3 == 0) && (i % 5 == 0)) {
+        System.out.println("Tutti-Frutti");
+      } 
+      if ((i % 3 != 0) && (i % 5 != 0)) {
+        System.out.println(i);
+      }		
+    }		
+  }
 }
