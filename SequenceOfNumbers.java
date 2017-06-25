9d4bb2d4d7d3321aa310e42635d96e00667d0624
RomanMiranovich <roman.miranovich@gmail.com>
2017-06-25 15:49:48 +0200

    Add files via upload

---
+++ SequenceOfNumbers.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SequenceOfNumbers {
+  public static void main(String[] args) {
+    System.out.println("Enter your sequence (use space between numbers):");
+    Scanner in = new Scanner (System.in);
+    String sequence = in.nextLine();
+    String numStr[] = sequence.split(" ");
+    int numArr[] = new int [numStr.length];
+    try {
+      for (int i = 0; i < numStr.length; i++) {
+        numArr[i] = Integer.parseInt(numStr[i]);
+      }
+    } catch (Exception e) {
+      System.out.println("You entered a wrong symbol. The program stops working");
+      return;
+    }
+    int lengthArr = numArr.length;
+    boolean checkSequence = true;
+    for (int i = 0; i + 1 < lengthArr; i++) {
+      if (numArr[i] > numArr[i+1]) {
+        checkSequence = false;
+        System.out.println("Your sequence is not non-decreasing");
+        break;
+      }
+    }   
+    if (checkSequence == true) {
+        System.out.println("Your sequence is non-decreasing");
+    }        
+  }
+}
