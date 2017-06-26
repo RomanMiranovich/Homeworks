110b954e88924f0c0dd8b1c306408133e2495e60
RomanMiranovich <roman.miranovich@gmail.com>
2017-06-26 13:00:42 +0200

    Update SequenceOfNumbers.java

--- SequenceOfNumbers.java
+++ SequenceOfNumbers.java
@@ -6,22 +6,24 @@ public class SequenceOfNumbers {
     Scanner in = new Scanner(System.in);
     String sequence = in.nextLine();
     String numStr[] = sequence.split(" ");
+    
     if (numStr.length == 1) {
       System.out.println("You entered only one number. It is not a sequence");
       return;
     }
+    
     try {
       for (int i = 0; i + 1 < numStr.length; i++) {
         if (Integer.parseInt(numStr[i]) > Integer.parseInt(numStr[i+1])) {
           System.out.println("Your sequence is not non-decreasing");
           break;
         } else if (i == numStr.length - 2) {
-        System.out.println("Your sequence is non-decreasing");
+          System.out.println("Your sequence is non-decreasing");
         }
       }
     } catch (Exception e) {
       System.out.println("You entered a wrong symbol. The program stops working");
-      return;
+      System.exit(0);
     }     
   }
 }
