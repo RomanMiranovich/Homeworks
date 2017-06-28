c486ef96f556b3f0f36dc34b20c2c0f7b374fe6b
RomanMiranovich <roman.miranovich@gmail.com>
2017-06-28 14:03:32 +0200

    Update SequenceOfNumbersModified.java

--- SequenceOfNumbersModified.java
+++ SequenceOfNumbersModified.java
@@ -1,32 +1,15 @@
 import java.util.Scanner;
 
 public class SequenceOfNumbersModified {
-  public static void main(String[] args) {
-    if (args.length == 0) {
-      System.out.println("You entered nothing with running program");
-      Scanner in = new Scanner(System.in);
-      System.out.println("Enter your sequence now (use space between numbers):");
-      String sequence = in.nextLine();
-      String numStr[] = sequence.split(" ");
-
-      if (numStr.length == 1) {
-        System.out.println("You entered only one number. It is not a sequence");
-        return;
-      }
-      
-      for (int i = 0; i < args.length; i++) {
-        args[i] = numStr[i];
-      }
-    }
-
-    if (args.length == 1) {
+  public static void сheckSequence(String[] newArgs) {
+    if (newArgs.length == 1) {
       System.out.println("You entered only one number. It is not a sequence");
       return;
     }
 
     try {  
-      for (int i = 0; i + 1 < args.length; i++) {
-        if (Integer.parseInt(args[i]) > Integer.parseInt(args[i+1])) {
+      for (int i = 0; i + 1 < newArgs.length; i++) {
+        if (Integer.parseInt(newArgs[i]) > Integer.parseInt(newArgs[i+1])) {
           System.out.println("Your sequence is not non-decreasing");
           return;
         }
@@ -34,6 +17,19 @@ public class SequenceOfNumbersModified {
     } catch (Exception e) {
       System.out.println("You entered a wrong symbol. The program stops working");
     }
-    System.out.println("Your sequence is non-decreasing");      
+    System.out.println("Your sequence is non-decreasing");
+  }
+  
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("You entered nothing with running program");
+      Scanner in = new Scanner(System.in);
+      System.out.println("Enter your sequence now (use space between numbers):");
+      String sequence = in.nextLine();
+      String numStr[] = sequence.split(" ");
+      сheckSequence(numStr);
+    } else {
+      сheckSequence(args);
+    }     
   }
 }
