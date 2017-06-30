44d7be086d8b85435b550b954dd0334754063f11
RomanMiranovich <roman.miranovich@gmail.com>
2017-06-30 13:11:30 +0200

    Update SequenceOfNumbersModified.java

--- SequenceOfNumbersModified.java
+++ SequenceOfNumbersModified.java
@@ -7,36 +7,40 @@ public class SequenceOfNumbersModified {
    * @param newArgs - array of strings which contain testing sequence. One elemet of array - one number
    */ 
   public void сheckSequence(String[] newArgs) {
+    final String POSITIVE_ANSWER = "Your sequence is non-decreasing";
+    final String NEGATIVE_ANSWER = "Your sequence is not non-decreasing";
+    final String WRONG_SYMBOL_ANSWER = "You entered a wrong symbol. The program stops working";
+    final String SINGLE_SYMBOL_ANSWER = "You entered only one number. It is not a sequence";
+    
     if (newArgs.length == 1) {
-      System.out.println("You entered only one number. It is not a sequence");
-      return;
+      return SINGLE_SYMBOL_ANSWER;
     }
 
     try {  
       for (int i = 0; i + 1 < newArgs.length; i++) {
         if (Integer.parseInt(newArgs[i]) > Integer.parseInt(newArgs[i+1])) {
-          System.out.println("Your sequence is not non-decreasing");
-          return;
+          return NEGATIVE_ANSWER;
         }
       }
     } catch (Exception e) {
-      System.out.println("You entered a wrong symbol. The program stops working");
-      return;
+      return WRONG_SYMBOL_ANSWER;
     }
-    System.out.println("Your sequence is non-decreasing");
+    return POSITIVE_ANSWER;
   }
   
   public static void main(String[] args) {
     SequenceOfNumbersModified object = new SequenceOfNumbersModified();
+    String answer = "0";
     if (args.length == 0) {
       System.out.println("You entered nothing arguments with starting program");
       Scanner in = new Scanner(System.in);
       System.out.println("Enter your sequence now (use space between numbers):");
       String sequence = in.nextLine();
       String numStr[] = sequence.split(" ");
-      object.сheckSequence(numStr);
+      answer = object.сheckSequence(numStr);
     } else {
-      object.сheckSequence(args);
-    }     
+      answer = object.сheckSequence(args);
+    }
+    System.out.println(answer);
   }
 }
