d9b92bb2d6d2f62ae786fa0948d2283e6287cbac
RomanMiranovich <roman.miranovich@gmail.com>
2017-07-02 14:36:28 +0200

    Update SequenceOfNumbers.java

--- SequenceOfNumbers.java
+++ SequenceOfNumbers.java
@@ -1,27 +1,46 @@
 import java.util.Scanner;
 
-public class SequenceOfNumbers {
-  public static void main(String[] args) {
-    System.out.println("Enter your sequence (use space between numbers):");
-    Scanner in = new Scanner(System.in);
-    String sequence = in.nextLine();
-    String numStr[] = sequence.split(" ");
+public class SequenceOfNumbersModified {
+  /** 
+   * The function is used for determination type of sequence: non-decreasing or not non-decreasing
+   *
+   * @param newArgs - array of strings which contain testing sequence. One elemet of array - one number
+   */ 
+  public String сheckSequence(String[] newArgs) {
+    final String POSITIVE_ANSWER = "Your sequence is non-decreasing";
+    final String NEGATIVE_ANSWER = "Your sequence is not non-decreasing";
+    final String WRONG_SYMBOL_ANSWER = "You entered a wrong symbol. The program stops working";
+    final String SINGLE_SYMBOL_ANSWER = "You entered only one number. It is not a sequence";
     
-    if (numStr.length == 1) {
-      System.out.println("You entered only one number. It is not a sequence");
-      return;
+    if (newArgs.length == 1) {
+      return SINGLE_SYMBOL_ANSWER;
     }
-    
-    try {
-      for (int i = 0; i + 1 < numStr.length; i++) {
-        if (Integer.parseInt(numStr[i]) > Integer.parseInt(numStr[i+1])) {
-          System.out.println("Your sequence is not non-decreasing");
-          return;
-        }        
+
+    try {  
+      for (int i = 0; i + 1 < newArgs.length; i++) {
+        if (Integer.parseInt(newArgs[i]) > Integer.parseInt(newArgs[i+1])) {
+          return NEGATIVE_ANSWER;
+        }
       }
     } catch (Exception e) {
-      System.out.println("You entered a wrong symbol. The program stops working");
+      return WRONG_SYMBOL_ANSWER;
     }
-    System.out.println("Your sequence is non-decreasing");
+    return POSITIVE_ANSWER;
+  }
+  
+  public static void main(String[] args) {
+    SequenceOfNumbersModified object = new SequenceOfNumbersModified();
+    String answer = "0";
+    if (args.length == 0) {
+      System.out.println("You entered nothing arguments with starting program");
+      Scanner in = new Scanner(System.in);
+      System.out.println("Enter your sequence now (use space between numbers):");
+      String sequence = in.nextLine();
+      String numStr[] = sequence.split(" ");
+      answer = object.сheckSequence(numStr);
+    } else {
+      answer = object.сheckSequence(args);
+    }
+    System.out.println(answer);
   }
 }
