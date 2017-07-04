292c47fdb6f2e1ad839d1202b49ced0e0e466eb5
RomanMiranovich <roman.miranovich@gmail.com>
2017-07-04 19:19:56 +0200

    Update SequenceOfNumbers.java

--- SequenceOfNumbers.java
+++ SequenceOfNumbers.java
@@ -1,42 +1,55 @@
-import java.util.Scanner;
-import java.util.Arrays;
-
 public class SequenceOfNumbers {
+  /** 
+   * The function is used for determination type of sequence: non-decreasing or not non-decreasing
+   *
+   * @param newArgs - array of String type which contain testing sequence. One elemet of array - one number
+   */  
+  public boolean сheckSequenceIsNonDecreasing(String[] newArgs) {
+    final String SINGLE_SYMBOL = "You entered only one number. It is not a sequence";
+    final String WRONG_SYMBOL = "You entered a wrong symbol. The program stops working"; 
+    try {
+      if (newArgs.length == 1) {
+        throw new IllegalArgumentException();
+      }
+      for (int i = 0; i + 1 < newArgs.length; i++) {
+        if (Integer.parseInt(newArgs[i]) > Integer.parseInt(newArgs[i + 1])) {
+          return false;
+        }
+      }
+    } catch (NumberFormatException e) {
+      System.out.println(WRONG_SYMBOL);
+      System.exit(13);
+    } catch (IllegalArgumentException e) {
+      System.out.println(SINGLE_SYMBOL);
+      System.exit(13);
+    }
+    return true;
+  }
+
   public static void main(String[] args) {
     final String WRONG_SYMBOL = "You entered a wrong symbol. The program stops working";
     final String NOTHING_ARGUMENTS = "You entered nothing arguments with starting program";
     final String ENTER_SEQUENCE = "Enter your sequence now (use space between numbers):";
     final String POSITIVE_ANSWER = "Your sequence is non-decreasing";
     final String NEGATIVE_ANSWER = "Your sequence is not non-decreasing";
-    final String SINGLE_SYMBOL = "You entered only one number. It is not a sequence";
-    String[] numStr;
-    
-    try { 
-      if (args.length == 0) {
-        System.out.println(NOTHING_ARGUMENTS);
-        Scanner in = new Scanner(System.in);
-        System.out.println(ENTER_SEQUENCE);
-        String sequence = in.nextLine();
-        numStr = sequence.split(" ");
-      } else {
-        numStr =  Arrays.copyOf(args, args.length);
-      }
-    
-      if (numStr.length == 1) {
-        System.out.println(SINGLE_SYMBOL);
-        return;
-      }
-    
-      for (int i = 0; i + 1 < numStr.length; i++) {
-        if (Integer.parseInt(numStr[i]) > Integer.parseInt(numStr[i + 1])) {
-          System.out.println(NEGATIVE_ANSWER);
-          return;
-        }
-      }
-    } catch (Exception e) {
-      System.out.println(WRONG_SYMBOL);
-      return;
+    SequenceOfNumbers object = new SequenceOfNumbers();
+    boolean checkAnswer = false;
+
+    if (args.length == 0) {
+      System.out.println(NOTHING_ARGUMENTS);
+      Scanner in = new Scanner(System.in);
+      System.out.println(ENTER_SEQUENCE);
+      String sequence = in.nextLine();
+      String[] numStr = sequence.split(" ");
+      checkAnswer = object.сheckSequenceIsNonDecreasing(numStr);
+    } else {
+      checkAnswer = object.сheckSequenceIsNonDecreasing(args);
     }
-    System.out.println(POSITIVE_ANSWER);
+    
+    if (checkAnswer) {
+      System.out.println(POSITIVE_ANSWER);
+    } else {
+      System.out.println(NEGATIVE_ANSWER);
+    }
   }
 }
