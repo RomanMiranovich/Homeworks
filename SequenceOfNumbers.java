c561205476dfe9bba2ed515f2e21f2c581f6e56c
RomanMiranovich <roman.miranovich@gmail.com>
2017-07-04 12:23:51 +0200

    Update SequenceOfNumbers.java

--- SequenceOfNumbers.java
+++ SequenceOfNumbers.java
@@ -4,43 +4,59 @@ public class SequenceOfNumbers {
   /** 
    * The function is used for determination type of sequence: non-decreasing or not non-decreasing
    *
-   * @param newArgs - array of strings which contain testing sequence. One elemet of array - one number
-   */ 
-  public String сheckSequence(String[] newArgs) {
+   * @param newArgs - array of String type which contain testing sequence. One element of array - one number
+   */  
+  public String сheckIsSequenceNonDecreasing(String[] newArgs) {
     final String POSITIVE_ANSWER = "Your sequence is non-decreasing";
     final String NEGATIVE_ANSWER = "Your sequence is not non-decreasing";
-    final String WRONG_SYMBOL_ANSWER = "You entered a wrong symbol. The program stops working";
-    final String SINGLE_SYMBOL_ANSWER = "You entered only one number. It is not a sequence";
     
-    if (newArgs.length == 1) {
-      return SINGLE_SYMBOL_ANSWER;
-    }
-
-    try {  
-      for (int i = 0; i + 1 < newArgs.length; i++) {
-        if (Integer.parseInt(newArgs[i]) > Integer.parseInt(newArgs[i+1])) {
-          return NEGATIVE_ANSWER;
-        }
+    for (int i = 0; i + 1 < newArgs.length; i++) {
+      if (Integer.parseInt(newArgs[i]) > Integer.parseInt(newArgs[i+1])) {
+        return NEGATIVE_ANSWER;
       }
-    } catch (Exception e) {
-      return WRONG_SYMBOL_ANSWER;
     }
     return POSITIVE_ANSWER;
   }
   
-  public static void main(String[] args) {
-    SequenceOfNumbersModified object = new SequenceOfNumbersModified();
-    String answer = "0";
-    if (args.length == 0) {
-      System.out.println("You entered nothing arguments with starting program");
-      Scanner in = new Scanner(System.in);
-      System.out.println("Enter your sequence now (use space between numbers):");
-      String sequence = in.nextLine();
-      String numStr[] = sequence.split(" ");
-      answer = object.сheckSequence(numStr);
-    } else {
-      answer = object.сheckSequence(args);
+  /**
+   * The function is used for checking if user entered only one number
+   *
+   *@param newArgs - array of String type which contain testing sequence. One element of array - one number
+   */  
+  public boolean checkIsOneSymbol(String[] newArgs) {
+    final String SINGLE_SYMBOL = "You entered only one number. It is not a sequence";
+    
+    if (newArgs.length == 1) {
+      System.out.println(SINGLE_SYMBOL);
+      return true;
     }
-    System.out.println(answer);
+  return false;
+  }
+    
+  public static void main(String[] args) {
+    final String WRONG_SYMBOL = "You entered a wrong symbol. The program stops working";
+    final String NOTHING_ARGUMENTS = "You entered nothing arguments with starting program";
+    final String ENTER_SEQUENCE = "Enter your sequence now (use space between numbers):";
+    String answerIsSequenceNonDecreasing = "0";
+    
+    SequenceOfNumbers object = new SequenceOfNumbers();
+    try {
+      if (args.length == 0) {
+        System.out.println(NOTHING_ARGUMENTS);
+        Scanner in = new Scanner(System.in);
+        System.out.println(ENTER_SEQUENCE);
+        String sequence = in.nextLine();
+        String numStr[] = sequence.split(" ");
+        if (!object.checkIsOneSymbol(numStr)) {
+          answerIsSequenceNonDecreasing = object.сheckIsSequenceNonDecreasing(numStr);
+          System.out.println(answerIsSequenceNonDecreasing);
+        }
+      } else if (!object.checkIsOneSymbol(args)) {
+        answerIsSequenceNonDecreasing = object.сheckIsSequenceNonDecreasing(args);
+        System.out.println(answerIsSequenceNonDecreasing);
+      }
+    } catch (Exception e) {
+      System.out.println(WRONG_SYMBOL);
+    }
   }
 }
